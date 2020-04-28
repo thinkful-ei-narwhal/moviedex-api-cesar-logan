8237ca9ad8deaf032f29df6d95f743c9f49d08af
Cesar Morales <cesarlenin2@live.com>
2020-04-28 12:51:45 -0700

    searching for genre works

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const movieData = require('./movie-data-small.json');
+const moviesData = require('./movies-data-small.json');
 const helmet = require('helmet');
 
 const app = express();
@@ -14,6 +14,29 @@ app.use(helmet());
 app.use(cors());
 
 console.log(process.env.API_TOKEN);
+
+// app.use(function validateBearerToken(req, res, next) {
+//   const apiToken = process.env.API_TOKEN;
+//   const authToken = req.get('Authorization');
+//   console.log(apiToken,authToken);
+//   if (!authToken || authToken.split(' ')[1] !== apiToken) {
+//     return res.status(401).json({ error: 'Unauthorized request' });
+//   }
+//   next();
+// });
+  
+app.get('/movie',(req,res)=>{
+  const {genre} = req.query;
+  let results=null;
+
+  if (genre) {
+    results= moviesData.filter(app=>
+      app.genre.toLocaleLowerCase().includes(genre.toLocaleLowerCase())
+    );
+  }
+
+  res.json(results);
+});
 
 const PORT = 8000;
 
