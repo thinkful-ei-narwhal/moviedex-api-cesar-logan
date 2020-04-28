b3d43e3c265a45def496d6cc4e6b61969645b75d
Cesar Morales <cesarlenin2@live.com>
2020-04-28 12:29:11 -0700

    finished blueprint

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 'use strict';
-console.log('hello');
+
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const movieData = require('./movie-data-small.json');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+console.log(process.env.API_TOKEN);
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
