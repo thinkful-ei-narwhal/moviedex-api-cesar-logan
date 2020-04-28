ca27b432f98b3a8a9b9c9db2f66c85a5437ed2ed
Cesar Morales <cesarlenin2@live.com>
2020-04-28 13:24:40 -0700

    uncommented auth api

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.use(cors());
 
 console.log(process.env.API_TOKEN);
 
-// app.use(function validateBearerToken(req, res, next) {
-//   const apiToken = process.env.API_TOKEN;
-//   const authToken = req.get('Authorization');
-//   console.log(apiToken,authToken);
-//   if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//     return res.status(401).json({ error: 'Unauthorized request' });
-//   }
-//   next();
-// });
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  console.log(apiToken,authToken);
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
 
 app.get('/movie', (req, res) => {
   const { genre, country, avg_vote } = req.query;
